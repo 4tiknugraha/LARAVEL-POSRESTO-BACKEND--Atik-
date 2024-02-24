9b7adb567f07e8f83cccf37d85111f73ec178d90
4tiknugraha <atiknugrah4@gmail.com>
2024-02-24 21:33:16 +0700

    update product

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Category;
+
+class CategoryController extends Controller
+{
+    //index
+    public function index(){
+        $categories = Category::paginate(10);
+        return view('pages.categories.index', compact('categories'));
+    }
+    //create
+    public function create(){
+        return view('pages.categories.create');
+    }
+    //store
+    public function store(Request $request){
+        //validate the request...
+        $request->validate([
+            'name'=>'required',
+            'image'=>'required|image|mimes:jpeg,jpg|,png,gif,svg,ico|max:2048',
+
+        ]);
+        $category = new Category();
+        $category->name = $request->name;
+        $category->description = $request->description;
+        // save image
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
+            $image->storeAs('public/categories', $category->id. '.'. $image->getClientOriginalExtension());
+            $category->image ='storage/categories/'. $category->id. '.'. $image->getClientOriginalExtension ();
+            $category->save();
+        }
+        return redirect()->route('categories.index')->with('success','Category created successfully');
+    }
+    //show
+    public function show($id){
+        return view('pages.categories.show');
+    }
+    //edit
+    public function edit($id){
+        $category = Category::find ($id);
+        return view('pages.categories.edit', compact('category'));
+    }
+    // update
+    public function update(Request $request, $id){
+        //validate the request...
+        $request->validate([
+            'name'=>'required',
+            'image'=>'required|image|mimes:jpeg,jpg|,png,gif,svg,ico|max:2048',
+
+        ]);
+        $category = Category::find ($id);
+        $category->name = $request->name;
+        $category->description = $request->description;
+        // save image
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
+            $image->storeAs('public/categories', $category->id. '.'. $image->getClientOriginalExtension());
+            $category->image ='storage/categories/'. $category->id. '.'. $image->getClientOriginalExtension ();
+            $category->save();
+        }
+        return redirect()->route('categories.index')->with('success','Category updated successfully');
+    }
+    // destroy
+    public function destroy($id){
+        // return view('pages.products.destroy');
+        // delete the request...
+        $category = Category::find ($id);
+        $category->delete();
+        return redirect()->route('categories.index')->with('success','Category deleted successfully');
+    }
+}
--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,7 +1,7 @@
 <div class="main-sidebar sidebar-style-2">
     <aside id="sidebar-wrapper">
         <div class="sidebar-brand">
-            <a href="index.html">KOPERASI</a>
+            <a href="index.html">ONLINE SHOP KOPERASI</a>
         </div>
         <div class="sidebar-brand sidebar-brand-sm">
             <a href="index.html">KP</a>
@@ -17,6 +17,41 @@
                 <ul class="dropdown-menu">
                     <li class=''>
                         <a class="nav-link" href="{{ route('users.index') }}">Users</a>
+                    </li>
+                </ul>
+                <ul class="dropdown-menu">
+                    <li class=''>
+                        <a class="nav-link" href="{{ route('products.index') }}">Products</a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+        <ul class="sidebar-menu">
+            <li class="nav-item dropdown">
+                <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Users</span></a>
+                <ul class="dropdown-menu">
+                    <li class='{{ Request::is('dashboard-general-dashboard') ? 'active' : '' }}'>
+                        <a class="nav-link" href="{{ url('dashboard-general-dashboard') }}">General Users</a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+        <ul class="sidebar-menu">
+            <li class="nav-item dropdown">
+                <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Category</span></a>
+                <ul class="dropdown-menu">
+                    <li class='{{ Request::is('dashboard-general-dashboard') ? 'active' : '' }}'>
+                        <a class="nav-link" href="{{ url('dashboard-general-dashboard') }}">General Category</a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+        <ul class="sidebar-menu">
+            <li class="nav-item dropdown">
+                <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Products</span></a>
+                <ul class="dropdown-menu">
+                    <li class='{{ Request::is('dashboard-general-dashboard') ? 'active' : '' }}'>
+                        <a class="nav-link" href="{{ url('dashboard-general-dashboard') }}">General Product</a>
                     </li>
                 </ul>
             </li>
